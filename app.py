d6ad05d5df18a3c9aee1b45dfce2d28192e09768
adio ihsan <adioihsan@gmail.com>
2023-05-12 19:24:48 +0700

    add since feature(year)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 from flask import Flask
 from flask import request
 from scrapper import twitter
@@ -13,8 +14,10 @@ def hello_word():
 def get_tweets():
     username = request.args.get('username')
     count = request.args.get('count')
+    since = request.args.get('since')
     if type(username) != str or len(username) < 3 :
         return json.dumps({"code":400,"message":"missing param /username/ or value to short. min length is 3 char "}), 400
+
     if type(count) == str:
         try:
             count = int(count)
@@ -24,15 +27,28 @@ def get_tweets():
             return json.dumps({"code":400,"message":"param /count/ should be integer."}), 400
     else:
         count = 50
-    tweets = twitter.get_only_tweets(username,count)
+    if type(since) == str:
+        year_today = int(datetime.date.today().year)
+        try:
+            since = int(since)
+            if   since > year_today   or since  <  2015:
+                return json.dumps({"code":400,"message":"param /since/  max  value is {} and min value is 2015.".format(year_today)}), 400
+        except:
+            return json.dumps({"code":400,"message":"param /since/ should be integer."}), 400
+    else:
+        since = 2015
+
+    tweets = twitter.get_only_tweets(username,count,since)
     return json.dumps(tweets)
 
 @app.route("/twitter/tweets/detail",methods=['GET'])
 def get_tweets_detail():
     username = request.args.get('username')
     count = request.args.get('count')
+    since = request.args.get('since')
     if type(username) != str or len(username) < 3 :
         return json.dumps({"code":400,"message":"missing param /username/ or value to short. min length is 3 char "}), 400
+
     if type(count) == str:
         try:
             count = int(count)
@@ -42,5 +58,17 @@ def get_tweets_detail():
             return json.dumps({"code":400,"message":"param /count/ should be integer."}), 400
     else:
         count = 50
-    tweets = twitter.get_detail_tweets(username,count)
+
+    if type(since) == str:
+        year_today = int(datetime.date.today().year)
+        try:
+            since = int(since)
+            if   since > year_today   or since  <  2015:
+                return json.dumps({"code":400,"message":"param /since/  max  value is {} and min value is 2015.".format(year_today)}), 400
+        except:
+            return json.dumps({"code":400,"message":"param /since/ should be integer."}), 400
+    else:
+        since = 2015
+
+    tweets = twitter.get_detail_tweets(username,count,since)
     return json.dumps(tweets,default=str)
--- scrapper/twitter.py
+++ scrapper/twitter.py
@@ -1,17 +1,21 @@
 import snscrape.modules.twitter as sntwitter
 
 
-def get_only_tweets(username,count=50):
+def get_only_tweets(username,count=50,since=2015):
     tweets=[]
-    for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:{}'.format(username)).get_items()):
+    since = "{}-01-01".format(since)
+    query = 'from:{} since:{}'.format(username,since)
+    for i,tweet in enumerate(sntwitter.TwitterSearchScraper(query).get_items()):
         if i >= count:
             break
         tweets.append(tweet.rawContent)
     return tweets
 
-def get_detail_tweets(username,count=50):
+def get_detail_tweets(username,count=50,since=2015):
     tweets=[]
-    for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:{}'.format(username)).get_items()):
+    since = "{}-01-01".format(since)
+    query = 'from:{} since:{}'.format(username,since)
+    for i,tweet in enumerate(sntwitter.TwitterSearchScraper(query).get_items()):
         if i >= count:
             break
         tweets.append({"id":tweet.id,"date":tweet.date,"username":tweet.user.username, "content":tweet.rawContent})
